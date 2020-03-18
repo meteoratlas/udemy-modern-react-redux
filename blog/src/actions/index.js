2a434678930a430f4454827cce07cd34ef262fd2
meteoratlas <kellyaccnts@gmail.com>
2020-03-17 23:49:30 -0600

    blog: finish project

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,27 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  // use lodash functions to only execute the network request once per user
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // dispatch an action from this action
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => async dispatch => {
   const resp = await jsonPlaceholder.get("/posts");
-  dispatch({ type: "FETCH_POSTS", payload: resp });
+  dispatch({ type: "FETCH_POSTS", payload: resp.data });
 };
+
+export const fetchUser = id => async dispatch => {
+  const resp = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: resp.data });
+};
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const resp = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: resp.data });
+// });
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
 
 export default combineReducers({
-  placeholder: () => "placeholder"
+  posts: postsReducer,
+  users: usersReducer
 });
