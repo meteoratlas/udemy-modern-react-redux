e955125658e3f6cfc52450887f36f12952f75f3d
meteoratlas <kellyaccnts@gmail.com>
2020-03-28 19:31:38 -0600

    finish streamlist component

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,7 +1,62 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
+import { Link } from "react-router-dom";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          {this.renderButtons(stream)}
+          <i className="large middle aligned icon camera" />
+          <div className="content">{stream.title}</div>
+          <div className="description">{stream.description}</div>
+        </div>
+      );
+    });
+  }
+  renderButtons(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div className={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
 };
 
-export default StreamList;
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
